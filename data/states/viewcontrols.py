51cb2cdb79487cc2939c9f200be76f4d7749ee29
Mekire <3ror@live.com>
2014-03-21 01:34:15 +0900

    Added controls display graphic.

--- data/states/viewcontrols.py
+++ data/states/viewcontrols.py
@@ -3,10 +3,7 @@ import pygame as pg
 from .. import prepare, tools
 
 
-FONT = pg.font.Font(prepare.FONTS["Fixedsys500c"], 20)
 FONT_BIG = pg.font.Font(prepare.FONTS["Fixedsys500c"], 60)
-
-BACKGROUND_COLOR = (63, 54, 50)
 
 
 class ViewControls(tools._State):
@@ -14,18 +11,15 @@ class ViewControls(tools._State):
     def __init__(self):
         tools._State.__init__(self)
         self.next = "SELECT"
-        self.timer = tools.Timer(200)
+        self.timer = tools.Timer(300)
         self.blink = False
-        msg_center = (prepare.SCREEN_RECT.centerx, 500)
-        self.ne_key = self.render_font(FONT, "[Press Any Key]",
-                                       pg.Color("yellow"), msg_center)
-        msg_center = (prepare.SCREEN_RECT.centerx, 50)
-        self.title = self.render_font(FONT_BIG, "Controls Screen Placeholder",
-                                      pg.Color("white"), msg_center)
+        msg_center = (prepare.SCREEN_RECT.centerx, 630)
+        self.ne_key = self.render_font(FONT_BIG, "Press Any Key",
+                                       pg.Color("white"), msg_center)
 
     def render_font(self, font, msg, color, center):
         """Return the rendered font surface and its rect centered on center."""
-        msg = font.render(msg, 1, color)
+        msg = font.render(msg, 0, color)
         rect = msg.get_rect(center=center)
         return msg, rect
 
@@ -35,7 +29,7 @@ class ViewControls(tools._State):
     def update(self, surface, keys, now, dt):
         if self.timer.check_tick(now):
             self.blink = not self.blink
-        surface.fill(BACKGROUND_COLOR)
-        surface.blit(*self.title)
+        surface.fill(prepare.BACKGROUND_COLOR)
+        surface.blit(prepare.GFX["misc"]["keyboard"], (0,0))
         if self.blink:
             surface.blit(*self.ne_key)
