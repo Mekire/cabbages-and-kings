4ad1764504262963d41c87eef921d2922ab7ee41
Mekire <3ror@live.com>
2014-04-02 10:06:12 +0900

    Some comments for modes.

--- data/components/modes.py
+++ data/components/modes.py
@@ -9,26 +9,29 @@ BASIC = ("base", "exttemple", "inttemple1", "inttemple2", "dungeon1",
 
 
 class Standard(object):
+    """Standard mode in the primary map editor."""
     def __init__(self, map_state):
         self.map_state = map_state
         self.make_panels()
         self.reset_add_del()
 
     def make_panels(self):
-        normal_pages = [panel.PanelPage(s, self.map_state) for s in BASIC]
-        self.panel = panel.Panel(self.map_state, normal_pages)
+        """Create necessary panels and their pages."""
+        pages = [panel.PanelPage(sheet, self.map_state) for sheet in BASIC]
+        self.panel = panel.Panel(self.map_state, pages)
         background_page = [panel.BackGroundPage(self.map_state)]
         self.background_panel = panel.Panel(self.map_state, background_page)
 
     @property
     def active_panel(self):
-        layer = self.map_state.layer
-        if layer != "BG Colors":
+        """Get the currently active panel (generally based on layer)."""
+        if self.map_state.layer != "BG Colors":
             return self.panel
         else:
             return self.background_panel
 
     def update(self, keys, now):
+        """Update panel and add/delete tiles from map editing window."""
         self.active_panel.update(keys, now)
         if self.adding:
             self.add_tile(pg.mouse.get_pos())
@@ -36,31 +39,33 @@ class Standard(object):
             self.del_tile(pg.mouse.get_pos())
 
     def get_event(self, event):
+        """
+        Handle mouse events on map editing window and pass event on to the
+        panel window.
+        """
         if event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if map_prepare.MAP_RECT.collidepoint(event.pos):
-                    self.set_adding(event.pos)
-            elif event.button == 3:
-                if map_prepare.MAP_RECT.collidepoint(event.pos):
-                    self.deleting = True
+            if map_prepare.MAP_RECT.collidepoint(event.pos):
+                if event.button == 1:
+                    self.set_add_del(event.pos, "adding")
+                elif event.button == 3:
+                    self.set_add_del(event.pos, "deleting")
         elif event.type == pg.MOUSEBUTTONUP:
             self.reset_add_del()
         self.active_panel.get_event(event)
 
-    def set_adding(self, point):
-        panel = self.active_panel
-        if not panel.rect.collidepoint(point):
-            self.adding = True
-            panel.retract()
+    def set_add_del(self, point, attribute):
+        """Set adding or deleting attributes and retract panel."""
+        if not self.active_panel.rect.collidepoint(point):
+            setattr(self, attribute, True)
+            self.active_panel.retract()
 
-    def set_deleting(self, point):
-        panel = self.active_panel
-        if not panel.rect.collidepoint(point):
-            self.adding = True
-            panel.retract()
+    def reset_add_del(self):
+        """Flip both adding and deleting back to False."""
+        self.adding = False
+        self.deleting = False
 
     def add_tile(self, point):
-        """Called if self.adding flag is set."""
+        """Called in update if self.adding flag is set."""
         selected = self.map_state.selected
         map_rect = map_prepare.MAP_RECT
         if selected:
@@ -70,6 +75,7 @@ class Standard(object):
                 self.map_state.map_dict[self.map_state.layer][point] = selected
 
     def del_tile(self, point):
+        """Called in update if self.deleting flag is set."""
         selected = self.map_state.selected
         map_rect = map_prepare.MAP_RECT
         if map_rect.collidepoint(point):
@@ -77,10 +83,6 @@ class Standard(object):
             point = tools.get_cell_coordinates(map_rect, point, size)
             self.map_state.map_dict[self.map_state.layer].pop(point, None)
 
-    def reset_add_del(self):
-        """Flip both adding and deleting back to False."""
-        self.adding = False
-        self.deleting = False
-
     def draw(self, surface, interpolate):
+        """Draw the currently active panell to the surface."""
         self.active_panel.draw(surface, interpolate)
